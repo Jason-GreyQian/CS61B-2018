fafed080f646ea4524e9261e718ca0f9120059c0
Jason-GreyQian <jason20011018@gmail.com>
2024-12-10 11:14:43 +0800

    finish A* and Cycle Check

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  *  @author Josh Hug
  */
@@ -20,7 +23,11 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int sourceX = maze.toX(v);
+        int sourceY = maze.toY(v);
+        int targetX = maze.toX(t);
+        int targetY = maze.toY(t);
+        return Math.abs(sourceX - targetX) + Math.abs(sourceY - targetY);
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -32,11 +39,59 @@ public class MazeAStarPath extends MazeExplorer {
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
         // TODO
+        // create the priority queue
+        NodeComparator cmp = new NodeComparator();
+        PriorityQueue<Node> pq = new PriorityQueue<>(cmp);
+        Node source = new Node(s);
+        pq.add(source);
+
+        while (!pq.isEmpty()) {
+            Node curr = pq.remove();
+            int v = curr.getV();
+
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    Node neighbour = new Node(w);
+                    edgeTo[neighbour.getV()] = v;
+                    distTo[neighbour.getV()] = distTo[v] + 1;
+                    announce();
+                    if (w == t) {
+                        targetFound = true;
+                        return ;
+                    }
+                    pq.add(neighbour);
+                }
+            }
+        }
+
     }
 
     @Override
     public void solve() {
         astar(s);
+    }
+
+    private class Node {
+        private int v;
+        private int priority;
+
+        Node(int v) {
+            this.v = v;
+            this.priority = distTo[v] + h(v);
+        }
+
+        int getV() {
+            return v;
+        }
+    }
+
+    private class NodeComparator implements Comparator<Node> {
+
+        @Override
+        public int compare(Node node, Node t1) {
+            return Integer.compare(node.priority, t1.priority);
+        }
     }
 
 }
